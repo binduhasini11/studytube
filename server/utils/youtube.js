735ff0e8b281adf11228de9545ede109f92e645f
Bindu Hasini <binduhasini11@gmail.com>
2026-05-10 11:21:31 +0530

    Add files via upload

---
+++ server/utils/youtube.js
@@ -0,0 +1,15 @@
+export function extractVideoId(url) {
+  const patterns = [
+    /(?:youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/embed\/)([^&\n?#]+)/,
+    /youtube\.com\/shorts\/([^&\n?#]+)/,
+  ];
+  for (const pattern of patterns) {
+    const match = url.match(pattern);
+    if (match) return match[1];
+  }
+  return null;
+}
+
+export function getThumbnailUrl(videoId) {
+  return `https://img.youtube.com/vi/${videoId}/maxresdefault.jpg`;
+}
